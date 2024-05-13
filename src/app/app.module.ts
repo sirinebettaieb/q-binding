c6b9bfee62d650bd1b2ec95cf16edc943eaa1100
SirineBETTAIEB <sirinebettaieb30@gmail.com>
2024-05-13 22:12:41 +0200

    modification

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+
+import { UserProfileComponent } from './user-profile/user-profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
-import { User } from './user.model'; // Import the 'User' type from the appropriate module.
+interface User {
+  name: string,
+  firstName: string,
+  age: number,
+  quote: string,
+  photo: string,
+  isHidden: boolean
+}
 
 @Component({
   selector: 'app-user-profile',
@@ -8,62 +15,23 @@ import { User } from './user.model'; // Import the 'User' type from the appropri
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.scss'
 })
+
 export class UserProfileComponent {
   user: User = {
-    photo: 'https://randomuser.me/api/portraits/lego/2.jpg',
-    quote: 'I am happy',
+    name: 'Doe',
+    firstName: 'John',
     age: 25,
-    firstName: 'Doe',
-    lastName: 'John',
-    hideAge: false
+    quote: 'Two beer or not to beer',
+    photo: 'https://randomuser.me/api/portraits/lego/2.jpg',
+    isHidden: false
   };
-photo: any;
-  constructor() {
-    this.user.age = 25;
-  }
-  ngOnInit() {
-    this.user.age = 25;
-  }
-  toggleAge() {
-    this.user.hideAge = !this.user.hideAge;
-  }
-  getAge() {
-    return this.user.age;
-  }
-  setAge(age: number) {
-    this.user.age = age;
-  }
-  getUser() {
-    return this.user;
-  }
-  setUser(user: User) {
-    this.user = user;
-  }
-  getFullName() {
-    return `${this.user.firstName} ${this.user.lastName}`;
-  }
-  setFullName(firstName: string, lastName: string) {
-    this.user.firstName = firstName;
-    this.user.lastName = lastName;
-  }
-  getPhoto() {
-    return this.user.photo;
-  }
-  setPhoto(photo: string) {
-    this.user.photo = photo;
-  }
-  getQuote() {
-    return this.user.quote;
-  }
-  setQuote(quote: string) {
-    this.user.quote = quote;
-  }
-  isHideAge() {
-    return this.user.hideAge;
-  }
-  setHideAge(hideAge: boolean) {
-    this.user.hideAge = hideAge;
-  }
 
+  sayHello() : void {
+    alert("hello !")
+  }
+  toggleHidden() : void {
+    this.user.isHidden = !this.user.isHidden;
+  }
 }
+ 
 
